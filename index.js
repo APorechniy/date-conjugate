447479fdbd507c105322bccf2e6884ff20856e3d
Alexandr Porechny <aa.porechny@makereal.ru>
2021-11-17 20:08:12 +0300

    init

---
+++ index.js
@@ -0,0 +1,45 @@
+const returned = (number, type) => {
+  if (Array.isArray(type)) {
+    if (lastNumber === '1') {
+      return `${number} ${type[0]}`
+    }
+
+    if (lastNumber === '2' || lastNumber === '3' || lastNumber === '4') {
+      return `${number} ${type[1]}`
+    }
+
+    if (
+      lastNumber === '5' ||
+      lastNumber === '6' ||
+      lastNumber === '7' ||
+      lastNumber === '8' ||
+      lastNumber === '9' ||
+      lastNumber === '0'
+    ) {
+      return `${number} ${type[2]}`
+    }
+  }
+  else {
+    console.log('Error: types not includes your type')
+  }
+}
+
+export const dateConjugate = (number, type) => {
+  const lastNumber = number.toString().slice(-1)
+  switch (type) {
+    case 'day':
+      const typeArray = ['день', 'дня', 'дней']
+      return returned(lastNumber, typeArray)
+      break;  
+    case 'month':
+      const typeArray = ['месяц', 'месяца', 'месяцев']
+      return returned(lastNumber, typeArray)
+      break;  
+    case 'year':
+      const typeArray = ['год', 'года', 'лет']
+      return returned(lastNumber, typeArray)
+      break;  
+    default:
+      break;
+  }
+}
